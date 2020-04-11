81567888a1d07439582656a919b7bdb2e340c67f
Rene Dahl <rene_dahl@hotmail.com>
2020-04-11 21:30:29 +0200

    working with sw

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,13 @@
+const filesToCache = ["", "index.html"];
+
+var staticCacheName = "skovfix-v1";
+
 self.addEventListener("install", function (event) {
   // Perform install steps
   console.log("Installing serviceworker..");
+  event.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
 });

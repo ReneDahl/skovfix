2ca3912e48329b3893c5783439ae99e7c41e80fc
Rene Dahl <rene_dahl@hotmail.com>
2020-04-12 14:07:52 +0200

    adding offline experience

--- script.js
+++ script.js
@@ -38,15 +38,3 @@ window.addEventListener("beforeinstallprompt", e => {
     });
   });
 });
-
-window.addEventListener("load", () => {
-  function handleNetworkChange(event) {
-    if (navigator.onLine) {
-      document.body.classList.remove("offline");
-    } else {
-      document.body.classList.add("offline");
-    }
-  }
-  window.addEventListener("online", handleNetworkChange);
-  window.addEventListener("offline", handleNetworkChange);
-});
